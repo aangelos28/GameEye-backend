507918cf9ca28b631d620d6e731f5a141f700429
Angelos Angelopoulos <aangelos28@gmail.com>
2020-08-20 14:28:11 -0400

    Initial restructuring

---
+++ common/src/test/java/edu/odu/cs411yellow/gameeyecommon/GameeyecommonApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.odu.cs411yellow.gameeyecommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameeyecommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
