59e95a5fbf2a620c2c93aab82a2b08fc35000f3c
jcoo <jcook006@odu.edu>
2020-09-15 01:52:47 -0400

    Add authentication database to application-test.properties and application.properties. Add JUnit 4.12 to build.gradle. Add basic test to insert/delete a game into the games collection (test runs successfully).

--- main-backend/src/test/java/edu/odu/cs411yellow/gameeyebackend/mainbackend/RepositoryTests.java
+++ main-backend/src/test/java/edu/odu/cs411yellow/gameeyebackend/mainbackend/RepositoryTests.java
@@ -1,13 +1,35 @@
 package edu.odu.cs411yellow.gameeyebackend.mainbackend;
 
+import edu.odu.cs411yellow.gameeyebackend.mainbackend.models.*;
+import edu.odu.cs411yellow.gameeyebackend.mainbackend.repositories.GameRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 @ActiveProfiles("test")
 @TestPropertySource(locations="classpath:application-test.properties")
 public class RepositoryTests {
+
+    @Autowired
+    private GameRepository gameRepository;
+
     @Test
-    public void testMongoConnection() {
+    public void GameRepositoryTest() {
+        // Create a game
+        Game game = new Game("1234", "Zelda", null, null, null, null, null, null);
+
+        // Save game in database
+        gameRepository.save(game);
+        assert(gameRepository.existsById("1234"));
+
+        // Delete game in database
+        gameRepository.delete(game);
+        assert(!gameRepository.existsById("1234"));
     }
 }
