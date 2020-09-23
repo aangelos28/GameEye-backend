7d9d7267c450a116ed376034e25952d4736e02d6
cdiasant <cdias002@odu.edu>
2020-09-23 14:02:16 -0400

    Setup Scraping of Articles
    Got Some Errors to Fix

--- main-backend/src/main/java/edu/odu/cs411yellow/gameeyebackend/mainbackend/webscrapers/GameSpotWebScraper.java
+++ main-backend/src/main/java/edu/odu/cs411yellow/gameeyebackend/mainbackend/webscrapers/GameSpotWebScraper.java
@@ -2,24 +2,17 @@ package edu.odu.cs411yellow.gameeyebackend.mainbackend.webscrapers;
 
 import edu.odu.cs411yellow.gameeyebackend.mainbackend.models.resources.Article;
 import org.jsoup.Jsoup;
-import org.w3c.dom.Document;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.util.List;
-
-/**
- * @author Chris
- */
 
 public class GameSpotWebScraper implements WebScraper {
 
     private String url = "https://www.gamespot.com/feeds/game-news/";
     private List<Article> articles;
-//    try {
-//        Document RssFeed = Jsoup.connect(url).userAgent("GameSpot RSS").get();
-//    }
-//    catch(Exception ex){
-//        ex.printStackTrace();
-//    }
+
 
 
     /**
@@ -36,7 +29,20 @@ public class GameSpotWebScraper implements WebScraper {
     @Override
     public void scrape() {
         try {
-            Document RssFeed = (Document) Jsoup.connect(url).userAgent("GameSpot RSS").get();
+            Document feed = Jsoup.connect(url).get();
+
+            Elements items = feed.select("item");
+
+            for (var i : items){
+
+                System.out.println(i.select("title").text());
+                System.out.println(i.select("link").text());
+                System.out.println(i.select("pubDate").text());
+                System.out.println(i.select("description").text());
+
+                //TODO Create List of Articles
+
+            }
         }
         catch(Exception ex){
             ex.printStackTrace();
---
+++ main-backend/src/test/java/edu/odu/cs411yellow/gameeyebackend/mainbackend/webscrapers/GameSpotWebScraperTests.java
@@ -0,0 +1,21 @@
+package edu.odu.cs411yellow.gameeyebackend.mainbackend.webscrapers;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GameSpotWebScraperTests {
+
+    @BeforeAll
+    public void setUp() {
+
+    }
+
+    @test
+    public void scrapTest() {
+
+        assert (true);
+    }
+
+}
