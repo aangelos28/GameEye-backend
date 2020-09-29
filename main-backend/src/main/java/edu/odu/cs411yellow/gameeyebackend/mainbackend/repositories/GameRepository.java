fde3d50288c90679b30b75d4685c7ae38a8ff637
jcoo <jcook006@odu.edu>
2020-09-29 01:02:00 -0400

    Rename findGameByTitle() to findByTitle().

--- main-backend/src/main/java/edu/odu/cs411yellow/gameeyebackend/mainbackend/repositories/GameRepository.java
+++ main-backend/src/main/java/edu/odu/cs411yellow/gameeyebackend/mainbackend/repositories/GameRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GameRepository extends MongoRepository<Game, String> {
-    Game findGameByTitle(String title);
+    Game findByTitle(String title);
 
     Game findGameById(String id);
 
