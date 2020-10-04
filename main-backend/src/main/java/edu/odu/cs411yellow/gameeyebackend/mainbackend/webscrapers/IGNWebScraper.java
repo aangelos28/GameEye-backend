72eefc1bfd865ed1f6ec1d777a838d0e0abcd8dc
blewis <blewi010@odu.edu>
2020-10-04 15:58:09 -0400

    Added database functionality

--- main-backend/src/main/java/edu/odu/cs411yellow/gameeyebackend/mainbackend/webscrapers/IGNWebScraper.java
+++ main-backend/src/main/java/edu/odu/cs411yellow/gameeyebackend/mainbackend/webscrapers/IGNWebScraper.java
@@ -1,12 +1,18 @@
 package edu.odu.cs411yellow.gameeyebackend.mainbackend.webscrapers;
-import edu.odu.cs411yellow.gameeyebackend.mainbackend.models.resources.Article;
-import edu.odu.cs411yellow.gameeyebackend.mainbackend.models.NewsWebsite;
-import edu.odu.cs411yellow.gameeyebackend.mainbackend.models.Image;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import edu.odu.cs411yellow.gameeyebackend.mainbackend.models.Image;
+import edu.odu.cs411yellow.gameeyebackend.mainbackend.models.NewsWebsite;
+import edu.odu.cs411yellow.gameeyebackend.mainbackend.models.resources.Article;
+import edu.odu.cs411yellow.gameeyebackend.mainbackend.repositories.NewsWebsiteRepository;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -14,12 +20,16 @@ import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
+@Service("IgnScrape")
 public class IGNWebScraper implements WebScraper{
 
-    private String url = "http://feeds.feedburner.com/ign/games-all";
+    private static final String url = "http://feeds.feedburner.com/ign/games-all";
     private List<Article> articles;
     private DateFormat format = new SimpleDateFormat("E, d MMMM yyyy kk:mm:ss z");
-    private String siteURL = "https://www.ign.com/";
+    //private String siteURL = "https://www.ign.com/";
+
+    @Autowired
+    NewsWebsiteRepository siteBuilder;
 
 
     public IGNWebScraper(List<Article> articles) {
@@ -33,16 +43,13 @@ public class IGNWebScraper implements WebScraper{
         try {
             //Connects to RSS feed and parses into a document to retrieve article elements
             Document doc = Jsoup.connect(url).get();
-            Date buildDate = format.parse(doc.selectFirst("pubDate").text());
+            NewsWebsite IGN= siteBuilder.findByName("IGN");
             Elements links = doc.getElementsByTag("item");  //A collection of articles from the parsed URL
 
             //Searches through each individual article
             for(Element link:links)
             {
-                String id = UUID.randomUUID().toString();   //Assigns a random ID number for article
-                NewsWebsite ign = new NewsWebsite(id,"IGN",null,siteURL,url,
-                        buildDate,buildDate);
-                Article curr = createArticle(link,ign);
+                Article curr = createArticle(link,IGN);
 
                 //Adds new Article to list if not already present in list
                 if(!checkDuplicateArticles(curr)) {
@@ -75,20 +82,18 @@ public class IGNWebScraper implements WebScraper{
             snippet = snippet.substring(0,255);
 
         int impact = 0;
+        String id = UUID.randomUUID().toString();   //Assigns a random ID number for article
+
+        Image thumbnail = new Image (id,".jpg",null);
 
         //TODO
-        //Calculate impact score
-        //
-        //Set article ID
-        //
         //Capture article Image
         //
         //Get Impact Score
 
 
-        Article curr= new Article(newsSite.getId(), title, source, newsSite, null,
+        return new Article(newsSite.getId(), title, source, newsSite, thumbnail,
                 snippet, pubDate, lastPubDate, impact);
-        return curr;
     }
 
     @Override
@@ -104,11 +109,22 @@ public class IGNWebScraper implements WebScraper{
 
     @Override
     public Boolean checkDuplicateArticles(Article a){
-        Boolean dupe = false;
+        for (Article i : articles) {
+            if (a.getTitle().contentEquals(i.getTitle()))
+                return true;
+        }
 
-        //TODO
-        //Check for duplicate articles
-
-        return dupe;
+        return false;
     }
+
+    /**
+     * Output to JSON format
+     * @return JSON
+     */
+    @Override
+    public String toString() {
+        Gson json = new GsonBuilder().setPrettyPrinting().create();
+        return json.toJson(this.articles);
+    }
+
 }
