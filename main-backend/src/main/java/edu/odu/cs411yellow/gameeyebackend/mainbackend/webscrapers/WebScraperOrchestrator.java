0b8f985fd6c2155c7e84bbfa2459ee25214b3132
blewis1014 <70607011+blewis1014@users.noreply.github.com>
2020-10-22 14:40:05 -0400

    Created class to orchestrate web scrapers

---
+++ main-backend/src/main/java/edu/odu/cs411yellow/gameeyebackend/mainbackend/webscrapers/WebScraperOrchestrator.java
@@ -0,0 +1,25 @@
+package edu.odu.cs411yellow.gameeyebackend.mainbackend.webscrapers;
+
+public class WebScraperOrchestrator {
+
+    public void forceScrape(){
+        //TODO
+    }
+
+    public void initiateBiDailyScrape(){
+        //TODO
+    }
+
+    public Boolean checkArticleDuplicates(){
+        return false;
+    }
+
+    public void insertDataIntoDatabase(){
+        //TODO
+    }
+
+    public void performArticleGameReferenceSearch(){
+        //TODO
+        //Consult Chris
+    }
+}
