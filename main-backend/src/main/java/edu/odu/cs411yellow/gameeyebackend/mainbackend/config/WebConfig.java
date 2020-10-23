f9a8083fad6f43297f3917a168ded7b8c91007d4
Angelos Angelopoulos <aangelos28@gmail.com>
2020-10-22 21:53:03 -0400

    Update CORS

--- main-backend/src/main/java/edu/odu/cs411yellow/gameeyebackend/mainbackend/config/WebConfig.java
+++ main-backend/src/main/java/edu/odu/cs411yellow/gameeyebackend/mainbackend/config/WebConfig.java
@@ -21,6 +21,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("*")
+                .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
     }
