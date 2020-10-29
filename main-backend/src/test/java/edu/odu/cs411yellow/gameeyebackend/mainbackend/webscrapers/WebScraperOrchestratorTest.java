244716963c72d5792f7a96cd5f28468093c60d72
blewis1014 <70607011+blewis1014@users.noreply.github.com>
2020-10-29 15:01:01 -0400

    Added unit tests for force scraping each individual scraper

--- main-backend/src/test/java/edu/odu/cs411yellow/gameeyebackend/mainbackend/webscrapers/WebScraperOrchestratorTest.java
+++ main-backend/src/test/java/edu/odu/cs411yellow/gameeyebackend/mainbackend/webscrapers/WebScraperOrchestratorTest.java
@@ -31,7 +31,7 @@ public class WebScraperOrchestratorTest {
     @Autowired
     EuroGamerScraper euro;
     @Autowired
-    GameSpotScraper gamespot;
+    GameSpotScraper gs;
 
     @Autowired
     WebScraperOrchestrator scrappy;
@@ -42,7 +42,7 @@ public class WebScraperOrchestratorTest {
 
     @BeforeEach
     public void init(){
-        scrappyMock = new WebScraperOrchestrator(mock, gamespot, ign, euro, pc, newsWebsiteRepository);
+        scrappyMock = new WebScraperOrchestrator(mock, gs, ign, euro, pc, newsWebsiteRepository);
         //scrappyMock = new WebScraperOrchestrator("GameEye Mock News");
         //WebScraper mock = new MockNewsScraper(newsWebsiteRepository);
         //scrappyMock = new WebScraperOrchestrator(mock);
@@ -57,10 +57,42 @@ public class WebScraperOrchestratorTest {
 
     @Test
     public void testForceScrapeMockNews(){
-        scrappyMock.forceScrape();
+        scrappyMock.forceScrape("GameEye Mock News");
         System.out.println(scrappyMock.toString());
         assertEquals(mock.toString(),scrappyMock.toString());
         assertEquals(mock.getArticles(), scrappyMock.getArticleCollection());
+    }
+
+    @Test
+    public void testForceScrapeGameSpot(){
+        scrappyMock.forceScrape("GameSpot");
+        System.out.println(scrappyMock.toString());
+        assertEquals(gs.toString(),scrappyMock.toString());
+        assertEquals(gs.getArticles(), scrappyMock.getArticleCollection());
+    }
+
+    @Test
+    public void testForceScrapeIGN(){
+        scrappyMock.forceScrape("IGN");
+        System.out.println(scrappyMock.toString());
+        assertEquals(ign.toString(),scrappyMock.toString());
+        assertEquals(ign.getArticles(), scrappyMock.getArticleCollection());
+    }
+
+    @Test
+    public void testForceScrapePCGamer(){
+        scrappyMock.forceScrape("PC Gamer");
+        System.out.println(scrappyMock.toString());
+        assertEquals(pc.toString(),scrappyMock.toString());
+        assertEquals(pc.getArticles(), scrappyMock.getArticleCollection());
+    }
+
+    @Test
+    public void testForceScrapeEuroGamer(){
+        scrappyMock.forceScrape("Eurogamer");
+        System.out.println(scrappyMock.toString());
+        assertEquals(euro.toString(),scrappyMock.toString());
+        assertEquals(euro.getArticles(), scrappyMock.getArticleCollection());
     }
 
     @Test
