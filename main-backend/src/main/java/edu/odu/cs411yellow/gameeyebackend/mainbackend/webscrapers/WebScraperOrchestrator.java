604a9d03ffc19ce6fb7fa160df2863f7f197a5f0
blewis1014 <70607011+blewis1014@users.noreply.github.com>
2020-10-28 23:57:50 -0400

    Cleaned up unneeded code and added @BeforeEach method for unit testing

--- main-backend/src/main/java/edu/odu/cs411yellow/gameeyebackend/mainbackend/webscrapers/WebScraperOrchestrator.java
+++ main-backend/src/main/java/edu/odu/cs411yellow/gameeyebackend/mainbackend/webscrapers/WebScraperOrchestrator.java
@@ -9,6 +9,8 @@ import edu.odu.cs411yellow.gameeyebackend.mainbackend.models.NewsWebsite;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
@@ -27,115 +29,41 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-
+@Service
 public class WebScraperOrchestrator{
 
     NewsWebsiteRepository newsWebsiteRepository;
 
     private List<WebScraper> scrapers;
-
     private List<Article> scrapedArticles;
 
-    @Autowired
     private MockNewsScraper mockNewsScraper;
+    private IGNScraper ignScraper;
+    private GameSpotScraper gameSpotScrapper;
+    private EuroGamerScraper euroGamerScraper;
+    private PCGamerScraper pcGamerScraper;
 
-    /*@Autowired
-    IGNScraper ignScraper;
     @Autowired
-    GameSpotScraper gameSpotScrapper;
-    @Autowired
-    EuroGamerScraper euroGamerScraper;
-    @Autowired
-    PCGamerScraper pcGamerScraper;*/
-
-
-    public WebScraperOrchestrator (){
+    public WebScraperOrchestrator (MockNewsScraper mockNewsScraper, NewsWebsiteRepository newsWebsiteRepository){
         this.scrapers = new ArrayList<WebScraper>();
         this.scrapedArticles = new ArrayList<Article>();
 
-        //this.ign = new IGNScraper(newsWebsiteRepository);
-        //this.gameSpot= new GameSpotScraper(newsWebsiteRepository);
-        //this.euroGamer= new EuroGamerScraper(newsWebsiteRepository);
-        //this.pcGamer = new PCGamerScraper(newsWebsiteRepository);
-        //this.mockSite = new MockNewsScraper(newsWebsiteRepository);
+        this.mockNewsScraper = mockNewsScraper;
+        this.newsWebsiteRepository = newsWebsiteRepository;
 
-
-        /*WebScraper ign = new IGNScraper(newsWebsiteRepository);
-        WebScraper gameSpot= new GameSpotScraper(newsWebsiteRepository);
-        WebScraper euroGamer= new EuroGamerScraper(newsWebsiteRepository);
-        WebScraper pcGamer = new PCGamerScraper(newsWebsiteRepository);
-        WebScraper mockSite = new MockNewsScraper(newsWebsiteRepository);*/
-
-        //WebScraper ign = ignScraper;
-        //WebScraper gameSpot = gameSpotScrapper;
-        //WebScraper euroGamer = euroGamerScraper;
-        //WebScraper pcGamer = euroGamerScraper;
-
-        WebScraper mockSite=mockNewsScraper;
 
         //scrapers.add(ign);
         //scrapers.add(gameSpot);
         //scrapers.add(euroGamer);
         //scrapers.add(pcGamer);
-        //scrapers.add(mockNewsScraper);
-        scrapers.add(mockSite);
+        scrapers.add(mockNewsScraper);
+
     }
 
-
-    public WebScraperOrchestrator (WebScraper target){
-        this.scrapers = new ArrayList<WebScraper>();
-        this.scrapedArticles = new ArrayList<Article>();
-
-        //WebScraper targetScraper = mockNewsScraper;
-
-        /*Boolean init = false;
-        switch (target){
-            case "IGN":
-                targetScraper = new IGNScraper(newsWebsiteRepository);
-                init = true;
-                break;
-            case "GameSpot":
-                targetScraper = new GameSpotScraper(newsWebsiteRepository);
-                init = true;
-                break;
-            case "Eurogamer":
-                targetScraper = new EuroGamerScraper(newsWebsiteRepository);
-                init = true;
-                break;
-            case "GameEye Mock News":
-                //targetScraper = new MockNewsScraper(newsWebsiteRepository);
-                targetScraper = mockNewsScraper;
-                init = true;
-                break;
-            case "PC Gamer":
-                targetScraper = new PCGamerScraper(newsWebsiteRepository);
-                init = true;
-                break;
-            default:
-                //targetScraper = new MockNewsScraper(newsWebsiteRepository); //Temporary default
-                targetScraper = mockNewsScraper;
-        }*/
-
-        /*if(init){
-            scrapers.add(targetScraper);
-        }
-        else{
-            throw new IllegalArgumentException("Invalid Scraper called");
-        }*/
-
-        //WebScraper mockSite=mockNewsScraper;
-        //scrapers.add(targetScraper);
-
-        //scrapers.add(mockNewsScraper);
-        //scrapers.add(mockSite);
-
-        scrapers.add(target);
-    }
 
     public void forceScrape(){
         for (WebScraper scraper:scrapers) {
             List<Article> articleList = scraper.scrape();
-            //List<Article> articleList = scraper.getArticles();
             for (Article art:articleList) {
                 if(!checkArticleDuplicates() && !checkIrrelevantArticles())
                     scrapedArticles.add(art);
@@ -143,7 +71,7 @@ public class WebScraperOrchestrator{
         }
     }
 
-    //@Scheduled (cron = "0 0 8,20 * * *")    //Schedules method to run at 8:00 AM and 8:00PM
+    @Scheduled (cron = "0 0 8,20 * * *")    //Schedules method to run at 8:00 AM and 8:00PM
     public void biDailyScrape(){
         //TODO
     }
--- main-backend/src/test/java/edu/odu/cs411yellow/gameeyebackend/mainbackend/webscrapers/WebScraperOrchestratorTest.java
+++ main-backend/src/test/java/edu/odu/cs411yellow/gameeyebackend/mainbackend/webscrapers/WebScraperOrchestratorTest.java
@@ -2,36 +2,45 @@ package edu.odu.cs411yellow.gameeyebackend.mainbackend.webscrapers;
 
 import edu.odu.cs411yellow.gameeyebackend.mainbackend.repositories.NewsWebsiteRepository;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.junit.Before;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.Assert;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class WebScraperOrchestratorTest {
 
     @Autowired
     MockNewsScraper mock;
 
+    @Autowired
     WebScraperOrchestrator scrappy;
+    @Autowired
     WebScraperOrchestrator scrappyMock;
+    @Autowired
     NewsWebsiteRepository newsWebsiteRepository;
 
-    @Before
+    @BeforeEach
     public void init(){
-        //scrappy = new WebScraperOrchestrator();
+        scrappyMock = new WebScraperOrchestrator(mock, newsWebsiteRepository);
         //scrappyMock = new WebScraperOrchestrator("GameEye Mock News");
         //WebScraper mock = new MockNewsScraper(newsWebsiteRepository);
         //scrappyMock = new WebScraperOrchestrator(mock);
     }
+
+
 
     @Test
     public void testForceScrape(){
@@ -40,17 +49,11 @@ public class WebScraperOrchestratorTest {
 
     @Test
     public void testForceScrapeMockNews(){
-        //WebScraper mock = new MockNewsScraper(newsWebsiteRepository);
-        WebScraper mockOrch = mock;
-        //scrappyMock = new WebScraperOrchestrator(mockOrch);
-        //scrappyMock = new WebScraperOrchestrator("GameEye Mock News");
-        scrappyMock = new WebScraperOrchestrator();
+        //scrappyMock = new WebScraperOrchestrator(mock, newsWebsiteRepository);
         scrappyMock.forceScrape();
-        //System.out.println(scrappyMock.getScrapers());
-        //System.out.println(scrappyMock.toString());
-        //Assert.noNullElements(scrappyMock.getArticleCollection(), "Error: No Articles Scraped");
-        //assertEquals(mock.toString(),scrappyMock.toString());
-        //assertEquals(mock.getArticles(), scrappyMock.getArticleCollection());
+        System.out.println(scrappyMock.toString());
+        assertEquals(mock.toString(),scrappyMock.toString());
+        assertEquals(mock.getArticles(), scrappyMock.getArticleCollection());
     }
 
     @Test
