a11cc78706278524f91364547285c1617ef35515
Angelos Angelopoulos <aangelos28@gmail.com>
2020-11-13 16:57:13 -0500

    Implement topic notification admin endpoint

--- main-backend/src/main/java/edu/odu/cs411yellow/gameeyebackend/mainbackend/controllers/NotificationController.java
+++ main-backend/src/main/java/edu/odu/cs411yellow/gameeyebackend/mainbackend/controllers/NotificationController.java
@@ -1,13 +1,10 @@
 package edu.odu.cs411yellow.gameeyebackend.mainbackend.controllers;
 
-import com.google.firebase.auth.FirebaseToken;
 import edu.odu.cs411yellow.gameeyebackend.mainbackend.services.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,12 +27,41 @@ public class NotificationController {
         public String imageUrl;
     }
 
-    @PostMapping(path = "/private-admin/notification/targeted-send", consumes = MediaType.APPLICATION_JSON_VALUE)
+    /**
+     * Sends a targeted notification to specific users.
+     *
+     * @param request HTTP request body.
+     */
+    @PostMapping(path = "/private-admin/notification/targeted/send", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> sendTargetedNotification(@RequestBody TargetedNotificationRequest request) {
         try {
             for (String userId : request.userIds) {
                 notifcationService.sendTargetedNotification(userId, request.title, request.body, request.imageUrl);
             }
+
+            return ResponseEntity.ok("Sent notification.");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    private static class TopicNotificationRequest {
+        public String topic;
+        public String title;
+        public String body;
+        public String imageUrl;
+    }
+
+    /**
+     * Sends a notification to all the subscribers of a topic.
+     *
+     * @param request HTTP request body.
+     */
+    @PostMapping(path = "/private-admin/notification/topic/send", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> sendTopicNotification(@RequestBody TopicNotificationRequest request) {
+        try {
+            notifcationService.sendTopicNotification(request.topic, request.title, request.body, request.imageUrl);
 
             return ResponseEntity.ok("Sent notification.");
         } catch (Exception ex) {
