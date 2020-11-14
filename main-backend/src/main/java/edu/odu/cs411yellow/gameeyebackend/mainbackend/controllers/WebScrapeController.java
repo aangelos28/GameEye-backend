c8ccc6d99df53bd3f2dac375b26717cd595d486b
cdiasant <cdias002@odu.edu>
2020-11-13 19:01:10 -0500

    updated WebScrapeController to add force scraping

--- main-backend/src/main/java/edu/odu/cs411yellow/gameeyebackend/mainbackend/controllers/WebScrapeController.java
+++ main-backend/src/main/java/edu/odu/cs411yellow/gameeyebackend/mainbackend/controllers/WebScrapeController.java
@@ -1,27 +1,15 @@
 package edu.odu.cs411yellow.gameeyebackend.mainbackend.controllers;
 
-import com.google.firebase.auth.FirebaseToken;
-import edu.odu.cs411yellow.gameeyebackend.mainbackend.models.elasticsearch.ElasticGame;
 import edu.odu.cs411yellow.gameeyebackend.mainbackend.webscrapers.MockNewsScraper;
 import edu.odu.cs411yellow.gameeyebackend.mainbackend.webscrapers.WebScraperOrchestrator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.elasticsearch.core.SearchHit;
-import org.springframework.data.elasticsearch.core.SearchHits;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-
+@RestController
 public class WebScrapeController {
 
     WebScraperOrchestrator webScraperOrchestrator;
@@ -31,7 +19,7 @@ public class WebScrapeController {
     MockNewsScraper mock;
 
     @Autowired
-    public WatchlistController (WebScraperOrchestrator webScraperOrchestrator){
+    public WebScrapeController (WebScraperOrchestrator webScraperOrchestrator){
         this.webScraperOrchestrator = webScraperOrchestrator;
     }
 
@@ -39,20 +27,20 @@ public class WebScrapeController {
      * Perform ForceScrape on RSS feeds
      *
      */
-    @GetMapping(path = "/private-admin/webscraping/force", String)
-    public ResponseEntity<?> performForceScrapeRSS()) {
+    @PostMapping(path = "/private-admin/webscraping/force")
+    public ResponseEntity<?> performForceScrapeRSS() {
         webScraperOrchestrator.forceScrape();
-
+        return ResponseEntity.ok("Force Scrape of RSS feeds Performed");
     }
 
     /**
      * Perform ForceScrape on RSS feeds
      *
      */
-    @GetMapping(path = "/private-admin/webscraping/mockwebsite/force", String)
-    public ResponseEntity<?> performForceScrapeMockSite()) {
+    @PostMapping(path = "/private-admin/webscraping/mockwebsite/force")
+    public ResponseEntity<?> performForceScrapeMockSite() {
         webScraperOrchestrator.forceScrape(mock);
-
+        return ResponseEntity.ok("Force Scrape of Mock News Performed");
     }
 
 }
